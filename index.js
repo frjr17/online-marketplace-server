755fa77be74f69f116c3321b8eba2c151d589e59
Hernan Valencia <frjr17@criptext.com>
2023-09-14 16:04:01 -0500

    fix: using dotenv directly

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const express = require('express');
-const dotenv = require('dotenv');
+require('dotenv').config({ path: "./.env.local" });
 
-dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
 
 app.get('/', (req, res) => {
-  res.send('Express + TypeScript Server');
+    res.send('Express + TypeScript Server');
 });
 
 app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+    console.log(`[server]: Server is running at http://localhost:${port}`);
 });
