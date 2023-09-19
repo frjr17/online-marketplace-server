50365c311a0a013202abedc291a427bc8286fb66
Hernán Valencia <frjr17@criptext.com>
2023-09-19 14:56:55 -0500

    feat: creating billing address model

--- models/billing.ts
+++ models/billing.ts
@@ -1,10 +1,28 @@
-import { Schema, model } from "mongoose";
+import { Schema, Types, model } from "mongoose";
+import { IUser } from "./user";
 
 export interface IBillingDetails {
-  // Define the properties of the BillingDetails interface here
+  fullName: string;
+  companyName: string;
+  streetAddress: string;
+  apartment?: string;
+  townOrCity: string;
+  phoneNumber: number;
+  email: string;
+  user: IUser;
 }
 const billingDetailsSchema = new Schema<IBillingDetails>({
-  // Define the schema for BillingDetails here
+  fullName: String,
+  companyName: String,
+  streetAddress: String,
+  apartment: String,
+  townOrCity: String,
+  phoneNumber: Number,
+  email: String,
+  user: {
+    type: Types.ObjectId,
+    ref: "User",
+  },
 });
 
 const BillingDetails = model<IBillingDetails>(
