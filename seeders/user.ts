bb1ff5a7a644c942b5d44a1f9d602f43d58d6e95
Hernán Valencia <frjr17@criptext.com>
2023-09-18 20:56:21 -0500

    feat: building user seeding

--- seeders/user.ts
+++ seeders/user.ts
@@ -1,15 +1,20 @@
-import { randUser } from "@ngneat/falso"
-import { IUser } from "../models/user"
-
-export const seedUsers = async ()=>{
-    for (let i =0; i<50;i++){
-        const userMainDetails = randUser()
-        console.log(userMainDetails)
-        const user: Partial<IUser> = {}
-        user.firstName =userMainDetails.firstName
-        user.lastName = userMainDetails.lastName
-        user.email = userMainDetails.email
-        
-
+import { randUser } from "@ngneat/falso";
+import { IUser } from "../models/user";
+import { hash } from "bcrypt";
+import { IList } from "../models/list";
+import { Types } from "mongoose";
+export const seedUsers = async () => {
+  for (let i = 0; i < 50; i++) {
+    const userMainDetails = randUser();
+    const user: Partial<IUser> = {};
+    user.firstName = userMainDetails.firstName;
+    user.lastName = userMainDetails.lastName;
+    user.email = userMainDetails.email;
+    user.password = await hash("whateverpassword1", 12);
+    user.profileImage = userMainDetails.img;
+    user.isSubscribed = false;
+    if (i === 1) {
+      console.log(user);
     }
-}
+  }
+};
