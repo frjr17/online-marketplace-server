e2a341556787118077fdb14ae58165fb46cefa48
Hernán Valencia <frjr17@criptext.com>
2023-09-20 00:02:57 -0500

    delete: remove users seed function to index

--- index.ts
+++ index.ts
@@ -1,7 +1,6 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-import { seedUsers } from "./seeders/user";
 
 dotenv.config({ path: ".env.local" });
 
@@ -14,7 +13,6 @@ app.get("/", (req: Request, res: Response) => {
 
 mongoose.connect(process.env.MONGO_DB_KEY as string).then(() => {
   app.listen(port, () => {
-    seedUsers()
     console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
   });
 });
