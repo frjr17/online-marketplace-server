aed7c3ec7e5d6913a5837a853506565d3158a8fb
Hernán Valencia <frjr17@criptext.com>
2023-09-22 20:54:39 -0500

    feat: adding category seeder

---
+++ seeders/category.ts
@@ -0,0 +1,20 @@
+import Category from "../models/category";
+
+export const seedCategories = async () => {
+  const categories = [
+    "Woman's Fashion",
+    "Men's Fahsion",
+    "Electronics",
+    "Home & Lifestyle",
+    "Medicine",
+    "Sports & Outdoor",
+    "Baby's & Toys",
+    "Groceries & Pets",
+    "Health & Beauty",
+  ];
+
+  for (let category of categories) {
+    const categoryObj = new Category({ name: category });
+    await categoryObj.save();
+  }
+};
