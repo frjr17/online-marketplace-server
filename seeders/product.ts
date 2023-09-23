f15aba493d829c39a85a965e8b80f004a3f5e691
Hernán Valencia <frjr17@criptext.com>
2023-09-22 23:23:42 -0500

    feat> adding seeders references 				

---
+++ seeders/product.ts
@@ -0,0 +1,37 @@
+import { randProduct, randProductName } from "@ngneat/falso";
+
+export const seedProducts = () => {
+  // Woman's Fashion
+  /** dummyjson.com
+   * womens-dresses
+   * womens-shoes
+   * women watches
+   * womens bags
+   */
+  // Men's Fashion
+  /** dummyjson.com
+   * mens-shirts
+   * mens-shoes
+   * mens-watches
+   *
+   */
+  // Electronics
+  /** dummyjson.com
+   * smartphones
+   * laptops
+   */
+  // Home & Lifestyle
+  /**dummyjson.com
+   *
+   * home-decoration
+   * furniture
+   */
+  // Medicine
+  // Sports & Outdoor
+  // Baby's & Toys
+  // Groceries & Pets
+  /**dummyjson.com
+   * groceries
+   */
+  // Health & Beauty
+};
