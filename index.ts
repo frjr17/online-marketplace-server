68ffd6e08e028718eedbbd697c26f5b3d45b20ba
Hernán Valencia <frjr17@criptext.com>
2023-09-25 21:56:56 -0500

    feat: adding express-file-routing for routing

--- index.ts
+++ index.ts
@@ -3,17 +3,15 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import { seedProducts } from "./seeders/product";
 import { seedDb } from "./seeders";
+import createRouter, { router } from "express-file-routing";
 
 dotenv.config({ path: ".env.local" });
 
 const app: Express = express();
 const port = process.env.PORT;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server is running");
-});
-
-mongoose.connect(process.env.MONGO_DB_KEY as string).then(() => {
+mongoose.connect(process.env.MONGO_DB_KEY as string).then(async () => {
+  await createRouter(app);
   app.listen(port, () => {
     // If you need to seed the database again...
     // seedDb();
