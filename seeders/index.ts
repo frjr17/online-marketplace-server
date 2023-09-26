08ba3ef915a074b474f05bb40243c6cb03184459
Hernán Valencia <frjr17@criptext.com>
2023-09-26 11:44:53 -0500

    fix: adding logs to main seeder

--- seeders/index.ts
+++ seeders/index.ts
@@ -6,10 +6,16 @@ import { seedProducts } from "./product";
 import { seedUsers } from "./user";
 
 export const seedDb = async () => {
+  console.log("Deleting users...");
   await User.deleteMany();
+  console.log("Deleting categories...");
   await Category.deleteMany();
+  console.log("Deleting products...");
   await Product.deleteMany();
+  console.log("Deleting carts...");
   await Cart.deleteMany();
+  console.log("Creating new users...");
   await seedUsers();
+  console.log("Creating new products and categories...");
   await seedProducts();
 };
