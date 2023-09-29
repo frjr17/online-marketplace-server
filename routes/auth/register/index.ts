be212faf4f3cc3fd6af4f9f053af1100985e839e
Hernán Valencia <frjr17@criptext.com>
2023-09-28 23:10:13 -0500

    fix: updating response body data

--- routes/auth/register/index.ts
+++ routes/auth/register/index.ts
@@ -25,6 +25,7 @@ export const post = async (
       isSubscribed,
       isLinkedWithGoogle,
     } = req.body;
+
     const user = new User({
       firstName,
       lastName,
@@ -47,9 +48,12 @@ export const post = async (
     await user.save();
     await cart.save();
 
-    return res
-      .status(200)
-      .json({ message: "User created!", success: true, user, token });
+    return res.status(200).json({
+      message: "User created!",
+      success: true,
+      user,
+      token: token.toJSON()._id,
+    });
   } catch (error) {
     console.error("Register Post error\n", error);
     res.send({ error: "There's some error" });
