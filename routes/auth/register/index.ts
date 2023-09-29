1bcc6c7b9eb7e41443b65e03570cbe704666fa07
Hernán Valencia <frjr17@criptext.com>
2023-09-28 23:40:42 -0500

    fix: adding registerValidator and checkValidators middlewares to register route

--- routes/auth/register/index.ts
+++ routes/auth/register/index.ts
@@ -2,6 +2,8 @@ import { Request, Response } from "express";
 import User from "../../../models/user";
 import Cart from "../../../models/cart";
 import RegisterToken from "../../../models/registerToken";
+import { registerValidator } from "../../../validators/auth";
+import { checkValidators } from "../../../validators";
 
 interface IPostRequestBody {
   firstName: string;
@@ -12,51 +14,52 @@ interface IPostRequestBody {
   isLinkedWithGoogle: boolean;
 }
 
-export const post = async (
-  req: Request<any, any, IPostRequestBody>,
-  res: Response
-) => {
-  try {
-    const {
-      firstName,
-      lastName,
-      email,
-      password,
-      isSubscribed,
-      isLinkedWithGoogle,
-    } = req.body;
+export const post = [
+  registerValidator(),
+  checkValidators,
+  async (req: Request<any, any, IPostRequestBody>, res: Response) => {
+    try {
+      const {
+        firstName,
+        lastName,
+        email,
+        password,
+        isSubscribed,
+        isLinkedWithGoogle,
+      } = req.body;
 
-    const user = new User({
-      firstName,
-      lastName,
-      email,
-      password,
-      isSubscribed,
-      isLinkedWithGoogle,
-      // Verification comes with token...
-      isVerified: false,
-    });
+      const user = new User({
+        firstName,
+        lastName,
+        email,
+        password,
+        isSubscribed,
+        isLinkedWithGoogle,
+        // Verification comes with token...
+        isVerified: false,
+      });
 
-    const cart = new Cart({
-      user,
-    });
+      const cart = new Cart({
+        user,
+      });
 
-    const token = new RegisterToken();
-    await token.save();
+      const token = new RegisterToken();
+      await token.save();
 
-    // Email part... for the moment, i'll just send the email token in the response (not secure)
+      // Email part... for the moment, i'll just send the email token in the response (not secure)
 
-    await user.save();
-    await cart.save();
+      await user.save();
+      await cart.save();
 
-    return res.status(200).json({
-      message: "User created!",
-      success: true,
-      user,
-      token: token.toJSON()._id,
-    });
-  } catch (error) {
-    console.error("Register Post error\n", error);
-    res.send({ error: "There's some error" });
-  }
-};
+      return res.status(200).json({
+        message: "User created!",
+        success: true,
+        user,
+        token: token.toJSON()._id,
+      });
+    } catch (error) {
+      console.error("Register Post error\n", error);
+      res.send({ error: "There's some error" });
+    }
+  },
+];
