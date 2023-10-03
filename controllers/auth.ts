ebbb43fa13f33097ef5a3eb733e8fd5adab6d2de
Hernán Valencia <frjr17@criptext.com>
2023-10-02 23:53:51 -0500

    fix: moving validateToken function to auth controller

--- controllers/auth.ts
+++ controllers/auth.ts
@@ -65,3 +65,48 @@ export const register = async (
     res.send({ error: "There's some error" });
   }
 };
+
+interface IPostRequestParams {
+  token: string;
+}
+
+export const validateToken = async (
+  req: Request<IPostRequestParams>,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { token } = req.params;
+    const registerToken = await RegisterToken.findById(token);
+
+    // In that case, I prefer to set the validators personally...
+
+    if (!registerToken) {
+      return res.status(400).send({ error: "Token does not exist" });
+    }
+
+    if (registerToken?.isUsed) {
+      return res.status(400).send({ error: "This token is used" });
+    }
+
+    const user = await User.findOne({ registerToken: token });
+
+    if (!user) {
+      return res.status(400).send({ error: "User doesn't exist" });
+    }
+
+    user.isVerfied = true;
+    await user.save();
+
+    // new JWT
+
+    req.state.data = {
+      user: {},
+    };
+    req.state.message = "User verified successfully";
+    return next();
+  } catch (error) {
+    console.error("Register Post error\n", error);
+    res.send({ error: "There's some error" });
+  }
+};
