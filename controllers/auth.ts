9034621789a4a90bd9cd35a25e2de9a922f85389
Hernán Valencia <frjr17@criptext.com>
2023-10-02 23:14:36 -0500

    feat: moving register route to new auth controller file

---
+++ controllers/auth.ts
@@ -0,0 +1,61 @@
+import { Request, Response } from "express";
+import User from "../models/user";
+import Cart from "../models/cart";
+import RegisterToken from "../models/registerToken";
+
+interface IPostRequestBody {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  isSubscribed: boolean;
+  isLinkedWithGoogle: boolean;
+}
+
+export const register = async (
+  req: Request<any, any, IPostRequestBody>,
+  res: Response
+) => {
+  try {
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      isSubscribed,
+      isLinkedWithGoogle,
+    } = req.body;
+
+    const user = new User({
+      firstName,
+      lastName,
+      email,
+      password,
+      isSubscribed,
+      isLinkedWithGoogle,
+      isVerified: false,
+    });
+
+    const cart = new Cart({
+      user,
+    });
+
+    const token = new RegisterToken();
+    await token.save();
+
+    // Email part... for the moment, i'll just send the email token in the response (not secure)
+
+    await user.save();
+    await cart.save();
+
+    return res.status(200).json({
+      message: "User created!",
+      success: true,
+      user,
+      token: token._id.toString(),
+    });
+  } catch (error) {
+    console.error("Register Post error\n", error);
+    res.send({ error: "There's some error" });
+  }
+};
