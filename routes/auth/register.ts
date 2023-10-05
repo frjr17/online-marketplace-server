ed9e3e2abd47961b3b5ab1b6c6eedba08ab3b770
Hernán Valencia <frjr17@criptext.com>
2023-10-05 16:25:23 -0500

    creating register post middleware

---
+++ routes/auth/register.ts
@@ -0,0 +1,6 @@
+import { sendState } from "../../controllers";
+import { register } from "../../controllers/auth/register";
+import { checkValidators } from "../../validators";
+import { registerValidator } from "../../validators/auth";
+
+export const post = [registerValidator(), checkValidators, register, sendState];
