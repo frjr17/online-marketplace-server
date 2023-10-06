c51d161cb8f81df77368094eb138bd8863f4d18a
Hernán Valencia <frjr17@criptext.com>
2023-10-06 09:59:01 -0500

    feat: creating login route

---
+++ controllers/auth/login.ts
@@ -0,0 +1,32 @@
+import { NextFunction, Request, Response } from "express";
+import User from "../../models/user";
+import LoginToken from "../../models/loginToken";
+
+export interface ILoginParams {}
+export interface ILoginResBody {}
+export interface ILoginReqBody {
+  email: string;
+  password: string;
+  isLinkedWithGoogle?: boolean;
+}
+export interface ILoginReqQuery {}
+
+export const login = async (
+  req: Request<ILoginParams, ILoginResBody, ILoginReqBody, ILoginReqQuery>,
+  res: Response<ILoginResBody>,
+  next: NextFunction
+) => {
+  try {
+    // extract data
+    const { email } = req.body;
+    const user = await User.findOne({ email });
+
+    const loginToken = await LoginToken.create({ user });
+
+    req.state.message = "User logged in successfully!";
+    return next();
+  } catch (error) {
+    console.error("login error\n", error);
+    next({ error, message: "Error login user" });
+  }
+};
